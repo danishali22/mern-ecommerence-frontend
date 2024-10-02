7488a0595e2cfc89f9a6eed2d3143f7a2f08d676
Danish Ali <dali27363@gmail.com>
2024-10-03 00:02:13 +0500

    "Added React state and functionality to Cart component, including coupon code input and validation"

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,7 +1,44 @@
-const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+import { useState } from "react";
+import { VscError } from "react-icons/vsc";
 
-export default Cart
+const cartItem = [];
+const subTotal = 4000;
+const tax = Math.round(subTotal * 0.18);
+const shippingCharges = 500;
+const discount = 400;
+const total = subTotal + tax + shippingCharges - discount;
+
+
+const Cart = () => {
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+  return (
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal: Rs {subTotal}</p>
+        <p>Shipping: Rs {shippingCharges}</p>
+        <p>Tax: Rs {tax}</p>
+        <p>
+          Discount: <em>- Rs {discount}</em>
+        </p>
+        <p>Total: Rs {total}</p>
+        <input
+          type="text"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+          placeholder="Coupon Code"
+        />
+        {
+          couponCode && (isValidCouponCode ? (
+            <span className="green">Rs {discount} is off using the <code>{couponCode}</code></span>
+          ) : (
+            <span className="red">Invalid Coupon <VscError /> </span>
+          ))
+        }
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
