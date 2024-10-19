99850cca1253af5fae49b1736380fe01ffdb1d5c
Danish Ali <dali27363@gmail.com>
2024-10-19 20:13:52 +0500

    "Added useEffect hook to redirect to cart page if cart is empty, and imported useSelector and useEffect from react"

--- src/pages/shipping.tsx
+++ src/pages/shipping.tsx
@@ -1,9 +1,22 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { CartReducerInitialState } from "../types/reducer-types";
 
 const Shipping = () => {
+  const { cartItems } = useSelector(
+    (state: { cartItem: CartReducerInitialState }) => state.cartItem
+  );
+
   const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (cartItems.length <= 0) {
+      return navigate("/cart");
+    }
+  }, [cartItems]);
+
 
   const [shippingInfo, setShippingInfo] = useState({
     address: "",
