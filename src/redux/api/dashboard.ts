1da52678c17d21e9ed1664a552a384d26d087ee2
Danish Ali <dali27363@gmail.com>
2024-10-22 00:29:09 +0500

    "Removed 'tagTypes' and 'providesTags' from dashboardApi, replaced with 'keepUnusedDataFor' in endpoints."

--- src/redux/api/dashboard.ts
+++ src/redux/api/dashboard.ts
@@ -4,23 +4,22 @@ import { BarChartResponse, LineChartResponse, PieChartResponse, StatsResponse } 
 export const dashboardApi = createApi({
     reducerPath: "dashboardApi",
     baseQuery: fetchBaseQuery({baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/dashboard`}),
-    tagTypes: ["dashboard"],
     endpoints: (builder) => ({
         stats: builder.query<StatsResponse, string>({
             query: (id) => `stats?id=${id}`,
-            providesTags: ["dashboard"],
+            keepUnusedDataFor: 0,
         }),
         pie: builder.query<PieChartResponse, string>({
             query: (id) => `pie?id=${id}`,
-            providesTags: ["dashboard"],
+            keepUnusedDataFor: 0,
         }),
         bar: builder.query<BarChartResponse, string>({
             query: (id) => `bar?id=${id}`,
-            providesTags: ["dashboard"],
+            keepUnusedDataFor: 0,
         }),
         line: builder.query<LineChartResponse, string>({
             query: (id) => `line?id=${id}`,
-            providesTags: ["dashboard"],
+            keepUnusedDataFor: 0,
         }),
     }),
 });
